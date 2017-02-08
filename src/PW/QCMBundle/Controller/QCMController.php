e9e597a421694dadc793bcf9acbd58ca5ca648e4
platinnson <joyplatini@gmail.com>
2017-02-08 13:52:43 +0100

    Ajout plusieurs QCM dans le mm groupe

--- src/PW/QCMBundle/Controller/QCMController.php
+++ src/PW/QCMBundle/Controller/QCMController.php
@@ -8,6 +8,9 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use PW\QCMBundle\Entity\QCM;
 use PW\QCMBundle\Form\QCMType;
+use PW\QCMBundle\Entity\QCMGroup;
+use PW\QCMBundle\Form\QCMGroupType;
+use PW\QCMBundle\Form\ImageType;
 
 class QCMController extends Controller
 {
@@ -72,11 +75,76 @@ class QCMController extends Controller
         	));
     }
 
+    public function add2Action($id, Request $request){
+
+        $qcmgroup = $this->getDoctrine()->getManager()
+                    ->getRepository('PWQCMBundle:QCMGroup')
+                    ->find($id);
+
+        $qcm = new QCM();
+
+        $qcm->setQcmgroup($qcmgroup);
+
+        $formBuilder = $this->get('form.factory')->createBuilder('form', $qcm);
+            
+        $formBuilder->add('enonce',     'textarea')
+                    ->add('propoA',     'textarea')
+                    ->add('propoB',     'textarea')
+                    ->add('propoC',     'textarea')
+                    ->add('propoD',     'textarea')
+                    ->add('propoE',     'textarea')
+                    ->add('reponse',    'choice', array(
+                        'choices' => array(
+                            'A' => 'Proposition A',
+                            'B' => 'Proposition B',
+                            'C' => 'Proposition C',
+                            'D' => 'Proposition D',
+                            'E' => 'Proposition E',
+                            ),
+                        'required' => true,
+                        'expanded' => false,
+                        'multiple' => false,
+                        ))
+                    ->add('explication', 'textarea')
+                    ->add('image',   new ImageType())
+                    ->add('submit',     'submit')
+                    ;
+
+        $form = $formBuilder->getForm();
+
+        if($request->isMethod('POST') && $form->handleRequest($request)->isValid()){
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($qcm);
+            $em->flush();
+
+            $request->getSession()->getFlashBag()
+                    ->add('notice', 'Annonce bien enregistree.');
+        
+            return $this->redirectToRoute('pw_qcm_add2', array(
+                'id' => $id
+                ));
+        }
+
+        return $this->render('PWQCMBundle:QCM:add2_qcm.html.twig', array(
+            'form' => $form->createView(),
+            'qcmgroup' => $qcm->getQcmgroup(),
+            ));
+    }
+
     public function editAction($id, Request $request){
 
         $qcm = $this->getDoctrine()->getManager()
                     ->getRepository('PWQCMBundle:QCM')
                     ->find($id);
+
+        $id2 = 7;
+
+        $qcmgroup = $this->getDoctrine()->getManager()
+                    ->getRepository('PWQCMBundle:QCMGroup')
+                    ->find($id2);
+
+        $qcm->setQcmgroup($qcmgroup);
 
         $form = $this->get('form.factory')->create(new QCMType(), $qcm);
             
